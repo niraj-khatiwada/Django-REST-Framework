ddf7c978f92f9e77e177aa558f9f9dfeb4dbfc56
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-05-06 21:06:42 +0545

    CRUDL

--- api_test/api.test.py
+++ api_test/api.test.py
@@ -2,7 +2,14 @@ import requests
 
 BASE_URL = 'http://localhost:8000/'
 ENDPOINT = 'api/'
-ID = 7
-response = requests.get(BASE_URL + ENDPOINT + str(ID))
+ID = 9
+a = {'pk': ID}
 
+
+def perform_request(ID, method='get'):
+    return requests.request(
+        method=method, url=BASE_URL + ENDPOINT, params=a, data=a)
+
+
+response = perform_request(9, 'get')
 print(response.json())
