cbc3f066b8a5e1ffa9f6dc3c06386c5c8f0db8d4
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-05-06 22:40:41 +0545

    CRUDL

--- api_test/api.test.py
+++ api_test/api.test.py
@@ -10,7 +10,7 @@ data = {'pk': 9}
 
 def perform_request(method='get'):
     return requests.request(
-        method=method, url=BASE_URL + ENDPOINT, data=json.dumps(data), headers={'content-type': 'application/json'})
+        method=method, url=BASE_URL + ENDPOINT, data=data, headers={'content-type': 'application/json'})
 
 
 response = perform_request('get')
