316ac651adf0cf47107f992d7082ea971f89f2ab
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-05-07 20:32:06 +0545

    JWT Auth

--- api_test/api.test.py
+++ api_test/api.test.py
@@ -29,18 +29,31 @@ logging.basicConfig(level=logging.DEBUG)
 # print(check_palindrome(string))
 BASE_URL = 'http://localhost:8000/'
 ENDPOINT = 'api/'
+AUTH = 'jwt-auth/'
 
 
-def send_request(method='get', ID=None, data=None):
+def send_request(method='get', ID=None, data=None, AUTH=None, token=None):
+    token = token if token is not None else {}
+    AUTH = AUTH if AUTH is not None else ''
     ID = str(ID) if ID is not None else ''
     data = data if data is not None else {}
     response = requests.request(
-        method=method, url=BASE_URL + ENDPOINT + ID, headers={'content-type': 'application/json'}, data=data)
+        method=method, url=BASE_URL + ENDPOINT + AUTH, headers={'content-type': 'application/json', 'Authorization': 'JWT' + token}, data=json.dumps(data))
     return response
 
 
 data = {
-    'content': 'Content from request'
+    'username': 'niraj',
+    'password': 'nepal123'
 }
-res = send_request(method='delete', ID=4).json()
-logging.info(res)
+token = send_request(method='post', data=data, AUTH=AUTH).json()
+logging.info(token)
+
+
+post_data = {
+    'content': 'Fresh content from Request'
+}
+
+res = send_request(method='post', data=post_data,
+                   token=token.get('token')).json()
+logging.debug(res)
