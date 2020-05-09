6fdc397ed5436ea42221a8ac24af037041c05d60
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-05-09 15:42:03 +0545

    JWT Auth Api View

--- api_test/api.test.py
+++ api_test/api.test.py
@@ -2,22 +2,38 @@ import requests
 import json
 
 BASE_URL = 'http://localhost:8000/api/'
+AUTH_ENDPOINT = 'obtain-jwt-auth/'
 
 
-def perform_request(method="get", data=None, ID='', headers={}):
-    data = {}
+def perform_request(method="get", data=None, ID='', headers={}, token=None, AUTH_ENDPOINT=''):
     # if data is not None:
     #     data = json.dumps(data)
+    #     headers['Content-Type'] = 'application/json'
+    # else:
+    #     data = {}
+    if token is not None:
+        headers['Authorization'] = "JWT " + token
+        print("headers", headers)
     response = requests.request(
-        method=method, url=BASE_URL, headers=headers, data=data)
+        method=method, url=BASE_URL + AUTH_ENDPOINT, headers=headers, data=data)
     return response
 
 
-data = {
-    'user': 1,
-    'content': 'Post from request',
-
+credentials = {
+    'username': 'niraj',
+    'password': 'nepal123'
 }
 
-print(perform_request(method="post", ID=8, data=data,
-                      headers={'content-type': 'application/json'}).json())
+token = perform_request(method="post", data=credentials,
+                        AUTH_ENDPOINT=AUTH_ENDPOINT).json()
+print(token)
+
+
+data = {
+    'content': 'Post from request',
+}
+
+post_data = perform_request(
+    method="post", data=data, token=token['token'])
+
+print(post_data.json())
